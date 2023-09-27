c48766a68a1beb136ccef28637fe116f530826b2
“rakibul8226” <rakibul8226@gmail.com>
2023-09-27 11:00:41 +0600

    clear comments

--- jS/index.js
+++ jS/index.js
@@ -1,73 +1,53 @@
 const handleCategory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await response.json();
-    // console.log(data);
 
     const tabContainer = document.getElementById("tab-container");
 
-    // console.log(data.data[0].category);
     data.data.forEach((element) => {
-        // console.log(element.category);
         const div = document.createElement("div");
-        div.innerHTML = `
-        
+        div.innerHTML = `      
         <a onclick="handleLoadCard(${element.category_id})" class="tab hover:bg-red-600 hover:text-white rounded-sm bg-slate-100">${element.category}</a>
-        `;
-        // console.log(element.category_id);
-       
+        `;      
         tabContainer.appendChild(div);
-    });
-
-    
-    
+    }); 
 }
 
 const handleLoadCard = async (categoryId) =>{
-    // console.log(categoryId);
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
-
-    // console.log(data);
+    
 
     cardContainer = document.getElementById("card-container");
-    // cardContainer.innerHTML
+    cardContainer.innerHTML="";
 
-    // console.log(data.data);
+        data.data.forEach((element) => {
+            const div = document.createElement("div");
+            div.innerHTML = `
 
-    data.data?.forEach((element) => {
-        // console.log(element.category_id);
-        console.log(element.thumbnail);
-        const div = document.createElement("div");
-        div.innerHTML = `
-
-        <div class="card w-auto h-80 bg-base-100 shadow-xl">
-        
-        
-            <figure><img class="h-60" src="${element.thumbnail}" alt=""></figure>
-        
-            <div class="card-body flex flex-row">
-
-                <div>
-                <img class="h-6 w-6 rounded-3xl mt-1" src="${element.authors[0].profile_picture}" alt="">
+            <div class="card w-auto h-80 bg-base-100 shadow-xl">
+                <figure><img class="h-60" src="${element.thumbnail}" alt=""></figure>  
+                <div class="card-body flex flex-row">
+                    <div>
+                    <img class="h-6 w-6 rounded-3xl mt-1" src="${element.authors[0].profile_picture}" alt="">
+                    </div>               
+                    <div ">   
+                        <h2 class="card-title font-semibold">${element.title}</h2>
+                        <p>${element.authors[0].profile_name}</p>
+                        <div class="flex">
+                        <p>${element.others.views} views ${element.authors[0].verified ? "<img class='h-6 w-6 inline'  src='verified.png'>":"<img src=''>"}</p>
+                        <p></p>
+                        </div>
+                    </div>
                 </div>
-
-                
-                <div ">   
-                    <h2 class="card-title font-semibold">${element.title}</h2>
-                    <p>${element.authors[0].profile_name}</p>
-                    <p>${element.others.views} views</p>
-                </div>
-            </div>
-        </div>
-        
-        `;
-
-        cardContainer.appendChild(div);
-    });
+            </div>      
+            `;
+            cardContainer.appendChild(div);
+        });
 
 }
 
 
-
 handleCategory();
+
 handleLoadCard(1000);
